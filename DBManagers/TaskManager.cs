269c41be936ca303e019c9bfd22663c729421d43
Ksenia <92778242+Antilevskaya-Ksenia-153501@users.noreply.github.com>
2023-05-05 22:14:29 +0300

    Creating managers, that will work with database

---
+++ DBManagers/TaskManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Calendar
+{
+    internal class TaskManager
+    {
+        private List<Task> tasks = new List<Task>();
+        public void AddTask(Task task)
+        {
+            tasks.Add(task);
+        }
+        public void UpdateTask(Task updatedTask)
+        {
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                if (tasks[i].TaskId == updatedTask.TaskId)
+                {
+                    tasks[i] = updatedTask;
+                    break;
+                }
+            }
+        }
+        public void RemoveTask(Task task)
+        {
+            tasks.Remove(task);
+        }
+        public void RemoveAllTasksOfUser(int userId)
+        {
+            foreach (Task task in tasks)
+            {
+                if (task.UserId == userId)
+                { 
+                    tasks.Remove(task);
+                }
+            }
+        }
+    }
+}
---
+++ DBManagers/UserManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Calendar
+{
+    internal class UserManager
+    {
+        private List<User> users = new List<User>();
+        public void AddUser(User user)
+        {
+            users.Add(user);
+        }
+        public void RemoveUser(User user)
+        {
+            users.Remove(user);
+        }
+        public User GetUserByEmail(string email)
+        {
+            foreach (User user in users)
+            {
+                if (user.Email == email)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+    }
+}
