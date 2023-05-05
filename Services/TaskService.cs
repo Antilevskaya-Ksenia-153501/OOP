2c6dbe70750890d7e28190c46bc7c49102e6f96f
Ksenia <92778242+Antilevskaya-Ksenia-153501@users.noreply.github.com>
2023-05-05 22:15:25 +0300

    Services will work with DB managers and users

---
+++ Services/TaskService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Calendar.Services
+{
+    internal class TaskService
+    {
+        private TaskManager TaskManager = null;
+        private Task CurrentTask = null;
+        public TaskService(TaskManager taskManager)
+        {
+            TaskManager = taskManager;
+        }
+        public void CreateTask(string name, string description, DateTime start, DateTime end, int currentUserId)
+        {
+            Task task = new Task(name, description, start, end, currentUserId);
+            TaskManager.AddTask(task);
+            CurrentTask = task;
+        }
+        public void SelectTask(Task task)
+        {
+            CurrentTask = task;
+        }
+        public void EditTaskTitle(string newTitle) 
+        { 
+            CurrentTask.Title = newTitle;
+            TaskManager.UpdateTask(CurrentTask);
+        }
+        public void EditTaskDescription(string newDescription)
+        {
+            CurrentTask.Description = newDescription;
+            TaskManager.UpdateTask(CurrentTask);
+        }
+        public void EditTaskStartTime(DateTime newStartTime)
+        {
+            CurrentTask.StartTime = newStartTime;
+            TaskManager.UpdateTask(CurrentTask);
+        }
+        public void EditTaskEndTime(DateTime newEndTime)
+        {
+            CurrentTask.EndTime = newEndTime;
+            TaskManager.UpdateTask(CurrentTask);
+        }
+        public void DeleteTask() 
+        {
+            TaskManager.RemoveTask(CurrentTask);
+            CurrentTask = null;
+        }
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Calendar.Services
+{
+    internal class UserService
+    {
+        private UserManager UserManager = null;
+        private TaskManager TaskManager = null;
+        public User CurrentUser { get; private set; }
+        public UserService(UserManager userManager, TaskManager taskManager)
+        {
+            UserManager = userManager;
+            TaskManager = taskManager;
+        }
+        public void SignUp(string email, string password)
+        {
+            User user = UserManager.GetUserByEmail(email);
+            if (user != null)
+            {
+                Console.WriteLine("Such user already exists. You should sign in.");
+            }
+            if (user == null)
+            {
+                user = new User(email, password);
+                UserManager.AddUser(user);
+                CurrentUser = user;
+            }
+        }
+        public void SignIn(string email, string password)
+        {
+            User user = UserManager.GetUserByEmail(email);
+            if (user != null)
+            {
+                if (user.Password == password)
+                {
+                    CurrentUser = user;
+                }
+            }
+        }
+        public void SignOut()
+        {
+            CurrentUser = null;
+        }
+        public void DeleteAccount()
+        {
+            TaskManager.RemoveAllTasksOfUser(CurrentUser.UserId);
+            UserManager.RemoveUser(CurrentUser);
+            CurrentUser = null;
+        }
+    }
+}
