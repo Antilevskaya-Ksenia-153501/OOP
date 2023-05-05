3629c15176b3313b18380e82216a8fd5ce8935d9
Ksenia <92778242+Antilevskaya-Ksenia-153501@users.noreply.github.com>
2023-05-05 22:13:40 +0300

    Creating entities of app

---
+++ Entities/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Calendar
+{
+    internal class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int TaskId { get; private set; }
+        public int UserId { get; private set; }
+        public Task(string title, string description, DateTime startTime, DateTime endTime, int userId)
+        {
+            Title = title;
+            Description = description;
+            StartTime = startTime;
+            EndTime = endTime;
+            TaskId = title.GetHashCode();
+            UserId = userId;
+        }
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Calendar
+{
+    internal class User
+    {
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public int UserId { get; private set; }
+
+        public User(string email, string password)
+        {
+            Email = email;
+            Password = password;
+            UserId = email.GetHashCode();
+        }
+    }
+}
